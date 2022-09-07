ea297057a167cdc12c34cb375cf97383a5098140
Vincent <vincentdenecken@outlook.com>
2022-09-07 14:00:35 +0200

    added files

---
+++ falling.js
@@ -0,0 +1,18 @@
+let elements = $(".solid");
+
+
+$(document).ready(function() {
+    for(let i = 0; i<5; i++) {
+
+
+    };
+
+
+
+
+    $(document).keydown(function() {
+
+    });
+});
+
+console.log($(".class"));
