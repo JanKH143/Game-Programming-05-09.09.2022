526191db2fa5c20cce463866e7d38db7fd1aa853
Alen <alen@pitic.net>
2022-09-07 14:38:06 +0200

    Update board.js

--- board.js
+++ board.js
@@ -2,6 +2,7 @@ let board = [];
 
 
 function generateBlock(solid, row, column) {
+	/*Befüllt die "unter Arrays" mit Objekten*/
 	return {
 		solid: solid,
 		row: row,
@@ -9,13 +10,38 @@ function generateBlock(solid, row, column) {
 	}
 }
 
+function showBoard (board) {
+	console.log(board.length);
+	/*Zeigt das Spielfeld an*/
+
+	for(let e = 0; e < board.length; e++){
+
+		for(let d = 0; d < board[e].length; d++) {
+
+
+			if (board[e][d].solid == true) {
+				/*Hier kommt Code zum anzeigen von Soliden Blocks rein*/
+			}
+			if (board[e][d].solid == false) {
+				/*Hier kommt Code zum anzeigen von nicht Soliden Blocks rein*/
+			}
+
+		}
+
+		
+	}
+}
+
 $(function() {
 	for(let i = 0; i < 20; i++) {
 
+		/*Erste for-Schleife kreirt die "unter Arrays"*/
 		board [i] = [];
 
 		if(i % 2 == 0) {
 			for(let j = 0; j < 38; j++) {
+
+				/*Zweite for-Schleife kreirt die "zweit Arrays"*/
 
 				if(j % 2  == 0) {
 					board [i][j] = generateBlock(true, i, j);
@@ -28,6 +54,8 @@ $(function() {
 		}
 		else {
 			for(let j = 0; j < 38; j++) {
+
+				/*Dritte for-Schleife kreirt auch "zweit Arrays"*/
 
 				if(j % 2 == 0) {
 					board [i][j] = generateBlock(false, i, j);
