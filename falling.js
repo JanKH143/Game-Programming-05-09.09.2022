020e81701110f26a66b1c201a0f34f2bd9743ac3
Vincent <vincentdenecken@outlook.com>
2022-09-07 14:06:58 +0200

    Update falling.js

--- falling.js
+++ falling.js
@@ -4,7 +4,7 @@ let element
 $(document).ready(function() {
     for(let i = 0; i<20; i++) {
         for(let j = 0; j<38; j++) {
-            element
+            element = spielfeld[]
 
         };
 
